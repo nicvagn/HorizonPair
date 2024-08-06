01b86a32e21f9b3ebc639a2488bfe130fd18d9e8
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-08-06 17:48:01 -0600

    dev: got QT working in cmake
    Also made first window

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
-//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 //
-//    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 //
-//    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+// You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
 #include <iostream>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-    cout << "Hello World";
+ QApplication app (argc, argv);
+
+ QPushButton button;
+ button.setText("My text");
+ button.setToolTip("A tooltip");
+ button.show();
+
+ return app.exec();
 }
